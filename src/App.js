fe84c6ef1be2823427ecf6563aaf738962d59fbb
Tre <tre@Tres-MacBook-Air.local>
2022-04-11 20:11:15 -0500

    splitting out cart-item into it's own component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import CategoryItem from './components/category-item/category-item.component';
 
+import './categories.styles.scss';
 
-function App() {
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ];
+
   return (
-    <div className="App">
-    Hello World!
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
